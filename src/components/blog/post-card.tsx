86ac248342c8bbc42370ba73c91656d796cf410a
Eduardo Abreu <eduardoabreu09@gmail.com>
2025-12-25 18:47:19 -0300

    WIP: Add link to post-card

--- src/components/blog/post-card.tsx
+++ src/components/blog/post-card.tsx
@@ -1,11 +1,9 @@
-"use client";
-
-import { useRouter } from "next/navigation";
 import { Card, CardFooter, CardHeader } from "@/components/ui/card";
 import type { BlogPostDTO } from "@/types/blog-post";
 import { CalendarDays, MessageSquareIcon, UserIcon } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { formatDateTime } from "@/lib/utils";
+import Link from "next/link";
 
 interface PostCardProps {
   post: BlogPostDTO;
@@ -13,54 +11,52 @@ interface PostCardProps {
 
 export function PostCard({ post }: PostCardProps) {
   const commentCount = post._count?.messages ?? 0;
-  const router = useRouter();
 
   return (
-    <Card
-      className="h-full w-full sm:max-w-2xl lg:max-w-3xl hover:bg-muted/50 transition-colors cursor-pointer flex flex-col"
-      onClick={() => {
-        router.push(`/home/blog/${post.id}`);
-      }}
-    >
-      <CardHeader>
-        <div className="mb-4">
-          <div className="flex flex-wrap gap-3 text-xs tracking-wider text-muted-foreground uppercase md:gap-5 lg:gap-6">
-            {post.tags &&
-              post.tags.map((tag) => <Badge key={tag.name}>{tag.name}</Badge>)}
+    <Card className="h-full w-full sm:max-w-2xl lg:max-w-3xl hover:bg-muted/50 transition-colors cursor-pointer flex flex-col">
+      <Link href={`/home/blog/${post.id}`}>
+        <CardHeader>
+          <div className="mb-4">
+            <div className="flex flex-wrap gap-3 text-xs tracking-wider text-muted-foreground uppercase md:gap-5 lg:gap-6">
+              {post.tags &&
+                post.tags.map((tag) => (
+                  <Badge key={tag.name}>{tag.name}</Badge>
+                ))}
+            </div>
           </div>
-        </div>
-        <div className="flex gap-1 items-center text-sm text-muted-foreground mb-2 md:text-base">
-          <CalendarDays size={16} />
-          <span>
-            {formatDateTime(post.createdAt, {
-              hour: "2-digit",
-              minute: "2-digit",
-            })}
-          </span>
-        </div>
-        <h3 className="mb-4 text-xl font-semibold md:text-2xl lg:text-3xl">
-          {post.title}
-        </h3>
-        <p className="mb-4 text-muted-foreground">{post.body}</p>
-        <div className="flex items-center gap-2 text-muted-foreground">
-          <span className="flex items-center gap-1">
-            <UserIcon size={16} />
-            {post.author.name}
-          </span>
-          {post.author.course && (
-            <>
-              <span>•</span>
-              <span>{post.author.course}</span>
-            </>
-          )}
-        </div>
-      </CardHeader>
-      <CardFooter className="text-sm text-muted-foreground">
-        <div className="flex items-center gap-1">
-          <MessageSquareIcon size={16} />
-          <span>{commentCount} comentários</span>
-        </div>
-      </CardFooter>
+          <div className="flex gap-1 items-center text-sm text-muted-foreground mb-2 md:text-base">
+            <CalendarDays size={16} />
+            <span>
+              {formatDateTime(post.createdAt, {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </span>
+          </div>
+          <h3 className="mb-4 text-xl font-semibold md:text-2xl lg:text-3xl">
+            {post.title}
+          </h3>
+          <p className="mb-4 text-muted-foreground">{post.body}</p>
+          <div className="flex items-center gap-2 text-muted-foreground">
+            <span className="flex items-center gap-1">
+              <UserIcon size={16} />
+              {post.author.name}
+            </span>
+            {post.author.course && (
+              <>
+                <span>•</span>
+                <span>{post.author.course}</span>
+              </>
+            )}
+          </div>
+        </CardHeader>
+        <CardFooter className="text-sm text-muted-foreground mt-4">
+          <div className="flex items-center gap-1">
+            <MessageSquareIcon size={16} />
+            <span>{commentCount} comentários</span>
+          </div>
+        </CardFooter>
+      </Link>
     </Card>
   );
 }
--- src/components/header/app-header.tsx
+++ src/components/header/app-header.tsx
@@ -2,11 +2,13 @@ import { Separator } from "@radix-ui/react-separator";
 import { AppBreadcrumb } from "./app-breadcrumb";
 import UserDropdown from "./user-dropdown";
 import { Suspense } from "react";
+import { SidebarTrigger } from "../ui/sidebar";
 
 export default function AppHeader() {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b">
       <div className="flex flex-1 items-center gap-2 px-3">
+        <SidebarTrigger className="-ms-4" />
         <Separator
           orientation="vertical"
           className="mr-2 data-[orientation=vertical]:h-4"
